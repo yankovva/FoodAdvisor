ad46f5ba0564972653ed57bbbf67d8cb0cf6bdd7
yankovva <84188264+MagdalenaYa@users.noreply.github.com>
2024-10-25 16:30:23 +0300

    Add project files.

---
+++ FoodAdvisor.Data/FoodAdvisorDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodAdvisor.Data
+{
+    public class FoodAdvisorDbContext : IdentityDbContext
+    {
+        public FoodAdvisorDbContext()
+        {
+            
+        }
+        public FoodAdvisorDbContext(DbContextOptions<FoodAdvisorDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+        }
+    }
+}
